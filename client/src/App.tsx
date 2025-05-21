bfdcd247fef5c56fb88120a2dcbe56ef659ebbc9
RBlaylock-Dev <rblaylock.prsvr@gmail.com>
2025-05-21 11:20:03 -0500

    feat:integrating landing components into the app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './sections/Header';
+import Hero from './sections/Hero';
+import Features from './sections/Features';
+import Footer from './sections/Footer';
 
 function App() {
-  
-
   return (
-    <>
-      <div >
-       
-      </div>
-       
-    </>
-  )
+    <Router>
+      <Header /> {/* This will render on every route */}
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/footer" element={<Footer />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
