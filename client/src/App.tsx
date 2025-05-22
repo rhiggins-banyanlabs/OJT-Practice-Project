e9d3fb85c7713aedac009ff5a1109c4943f2cb57
RBlaylock-Dev <rblaylock.prsvr@gmail.com>
2025-05-22 11:02:17 -0500

    bugfix: fixed an issue with routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './sections/Header';
 import Hero from './sections/Hero';
 import Features from './sections/Features';
 import Footer from './sections/Footer';
 
-
 function App() {
   return (
-    <Router>
-      <Header /> {/* This will render on every route */}
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/features" element={<Features />} />
-        <Route path="/footer" element={<Footer />} />
-      </Routes>
-    </Router>
+    <>
+      <Header />
+      <main className="pt-[80px]"> {/* to prevent content under fixed header */}
+        <Hero />
+        <Features />
+        <Footer />
+      </main>
+    </>
   );
-}
+};
 
 export default App;
