529f7b0bc4eb5df51332c7c0312a240b29b55359
dwalker-banyanlabs <dwalker@banyanlabs.io>
2025-05-22 11:42:25 -0400

    feat:Hero done except pic

---
+++ client/src/sections/Hero.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Header from '../components/ui/Heading.tsx';
+import Paragraph from '../components/ui/Paragraph.tsx';
+import Button from '../components/ui/Button.tsx';
+
+const Card = () => {
+  return (
+    <div className="flex items-center justify-between gap-8 p-4">
+      {/* Left: Text content */}
+      <div className="flex-1">
+        <Header
+            level="h1"
+        >
+          Step Into Comfort & Style
+        </Header>
+        <br />
+        <Paragraph
+            variant="body"
+        >
+          Discover our new collection of premium shoes designed for both comfort and style. Crafted with the finest materials and attention to detail.
+        </Paragraph>
+        <br />
+        <Button>
+          Shop Collection
+        </Button>
+        <Button
+          variant="link"
+          className="cursor:pointer ml-4"
+        >
+          Learn More
+        </Button>
+      </div>
+
+      {/* Right: Image */}
+      <div className="flex-1">
+        <img src="/image.png" alt="Shoe Collection" className="w-full h-auto object-contain" />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
